e747cc6fe1b640422bda1f80890b2a2664139e34
1xxz188 <1009159531@qq.com>
2025-12-13 18:28:34 +0800

    完善test_client

--- test_client/main.go
+++ test_client/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -9,4 +11,9 @@ func main() {
 	if err != nil {
 		fmt.Printf("示例运行失败: %v\n", err)
 	}
+
+	// 按任意键退出
+	fmt.Println("\n按回车键退出...")
+	reader := bufio.NewReader(os.Stdin)
+	_, _ = reader.ReadByte()
 }
